6dbc575825a5b6349547f4c023ac82d9fdd53fff
Renfrid <renfridfrancis@gmail.com>
2020-11-05 19:38:28 +0300

    Minor changes

--- modules/questionnare/admin.py
+++ modules/questionnare/admin.py
@@ -59,14 +59,14 @@ class SectionAdmin(admin.ModelAdmin):
 #Question List
 @admin.register(QuestionList)
 class QuestionListAdmin(admin.ModelAdmin):
-    list_display = ['id', 'code','title', 'has_sub']
-    list_filter = ['has_sub']
-    ordering = ("sort_order",)
+    list_display = ['code','title', 'qn_type', 'has_sub']
+    list_filter = ['code']
+    ordering = ("code",)
 
 
 #Question Bank  
 @admin.register(QuestionBank)
 class QuestionBankAdmin(admin.ModelAdmin):
-    list_display = ['sort_order','question',]  
+    list_display = ['sort_order','question']  
     list_filter  = ['section']
-    ordering = ("sort_order",)
+    ordering = ("section", "sort_order")
