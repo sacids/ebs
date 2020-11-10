512a4573e8fd726a0ca578bf6b6e7d02532fe1f0
mchizani <ericbeda@gmail.com>
2020-11-10 10:54:58 +0300

    changed styles and commented user_profiel in views

--- modules/questionnare/views.py
+++ modules/questionnare/views.py
@@ -85,11 +85,11 @@ def section_two(request):
     questions = QuestionList.objects.filter(section_id=2)
 
     # user
-    user_profile = Profiles.objects.get(user_id=request.user.id)
+    #user_profile = Profiles.objects.get(user_id=request.user.id)
 
     # context
     context = {
-        "user": user_profile,
+        #"user": user_profile,
         "questions": questions
     }
 
@@ -109,7 +109,7 @@ def section_two(request):
                 #save or update
                 AnsBank.objects.update_or_create(
                     created_by_id=request.user.id,
-                    country_id=user_profile.country_id,
+                    country_id=request.user.profile.country_id,
                     question_id=question.id,
                     answer=answer,
                     remarks=remarks
@@ -130,7 +130,7 @@ def section_three(request):
 
     # context
     context = {
-        "user_id": request.user.id,
+        #"user_id": request.user.id,
         "questions": questions
     }
 
@@ -171,7 +171,7 @@ def section_four(request):
 
     # context
     context = {
-        "user_id": request.user.id,
+        #"user_id": request.user.id,
         "questions": questions
     }
 
@@ -212,7 +212,7 @@ def section_five(request):
 
     # context
     context = {
-        "user_id": request.user.id,
+        #"user_id": request.user.id,
         "questions": questions
     }
 
