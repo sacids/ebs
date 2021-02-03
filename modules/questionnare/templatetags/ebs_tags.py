e1437c6b5749421c9b8adc027a88e6e6033302f7
Renfrid <renfridfrancis@gmail.com>
2021-02-03 12:38:35 +0300

    Show logout button

--- modules/questionnare/templatetags/ebs_tags.py
+++ modules/questionnare/templatetags/ebs_tags.py
@@ -12,6 +12,10 @@ register = template.Library()
 def get_by_index(l, i):
     return l[i]
 
+@register.filter(name='has_group') 
+def has_group(user, group_name):
+    return user.groups.filter(name=group_name).exists()
+
 
 @register.filter
 def get_qn_title(qn_id):
