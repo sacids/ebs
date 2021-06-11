38ad44b69c7b11cefdc4007d48677707d7bca1f2
multics2018 <ngolongolo@multics.co.tz>
2021-06-11 10:34:17 +0300

    add field description on transaltion

--- modules/questionnare/translation.py
+++ modules/questionnare/translation.py
@@ -11,7 +11,7 @@ class QuestionListTransalationOptions(TranslationOptions):
 
 
 class SurveyTransalationOptions(TranslationOptions):
-    fields = ("title",)    
+    fields = ("title", 'description', )    
 
 
 translator.register(Section, SectionTranslationOptions)    
