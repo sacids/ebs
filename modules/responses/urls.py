17c3eb4bd4620d7e7df2338987de58e6591cc2e5
renfrid <ngolongolo@multics.co.tz>
2022-06-16 14:09:27 +0300

    Modify viewing of responses

--- modules/responses/urls.py
+++ modules/responses/urls.py
@@ -6,22 +6,8 @@ app_name = 'responses'
 urlpatterns = [
     # admin
     path('surveys/', views.surveys, name='surveys'),
-    path('countries/<int:survey_id>', views.countries, name='countries'),
-
-    #survey 1
-    path("section_one/<int:country_id>", views.section_one, name="section_one"),
-    path("section_two/<int:country_id>", views.section_two, name="section_two"),
-    path("section_three/<int:country_id>", views.section_three, name="section_three"),
-    path("section_four/<int:country_id>", views.section_four, name="section_four"),
-    path("section_five/<int:country_id>", views.section_five, name="section_five"),
-    path("section_six/<int:country_id>", views.section_six, name="section_six"),
-
-    #survey 2
-    path("general_info/<int:country_id>", views.general_info, name="general_info"),
-    path("metrics/<int:country_id>", views.metrics, name="metrics"),
-    path("preference/<int:country_id>", views.preference, name="preference"),
-    path("information/<int:country_id>", views.information, name="information"),
-
+    path('countries/', views.countries, name='countries'),
+    path('questions/', views.questions, name='questions'),
 
     # alerts
     path('send_incomplete_submission_alert/<int:survey_id>/<int:country_id>',
--- modules/responses/views.py
+++ modules/responses/views.py
@@ -1,5 +1,4 @@
 import csv
-
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse_lazy, reverse
@@ -10,11 +9,10 @@ from django.db.models.query import Prefetch
 from ..notification.views import send_notification
 from django.shortcuts import redirect, render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
-
 from datetime import datetime
 from datetime import timedelta
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
@@ -38,35 +36,42 @@ def surveys(request):
 # countries
 @login_required(login_url='/login')
 def countries(request, **kwargs):
-    # survey
-    try:
-        survey = Survey.objects.get(pk=kwargs['survey_id'])
+     # survey
+    survey_id = request.GET.get('sv_id', -1)
+    survey = get_object_or_404(Survey.objects.all(), pk=survey_id)
 
-        # countries
-        countries = Country.objects.all().order_by('id')
+    countries = Country.objects.all().order_by('id')
 
-        # context
-        context = {
-            "survey": survey,
-            "countries": countries
-        }
-        # render view
-        return render(request, "lists.html", context)
-    except:
-        pass
+    context = {
+        "survey": survey,
+        "countries": countries
+    }
+
+    # render view
+    return render(request, "lists.html", context)
 
 
-# ===========================================================
-# SURVEY 1
-# ===========================================================
-# section one
 @login_required(login_url='/login')
-def section_one(request, **kwargs):
+def questions(request):
     # survey
-    survey = Survey.objects.get(pk=1)
+    survey_id = request.GET.get('sv_id', -1)
+    survey = get_object_or_404(Survey.objects.all(), pk=survey_id)
+
+    #section_id
+    section_id = request.GET.get('sc_id', -1)
+    country_id = request.GET.get('c_id')
+
+    if section_id == -1:
+        section = Section.objects.filter(survey_id=survey_id).order_by("code")[0]
+    else:
+        section = get_object_or_404(Section.objects.all(), pk=section_id)
+
+    #prev and next section
+    prev_section_id = prev_section(survey.id, section.id)
+    next_section_id = next_section(survey.id, section.id)    
 
     # country
-    country = Country.objects.get(pk=kwargs['country_id'])
+    country = Country.objects.get(pk=country_id)
 
     # profile
     try:
@@ -75,330 +80,63 @@ def section_one(request, **kwargs):
     except:
         profile = []
         user = []
-        pass
+        pass  
 
-   # questions
-    questions = QuestionList.objects.filter(section_id=1)
+    #questions
+    questions = QuestionList.objects.filter(section_id = section.id).order_by("code", "sort_order")  
 
-    # context
+     # context
     context = {
         "survey": survey,
-        "questions": questions,
-        "country": country,
-        "profile": profile,
-        "user": user
-    }
-
-    # render view
-    return render(request, "sections/one.html", context)
-
-
-# section two
-@login_required(login_url='/login')
-def section_two(request, **kwargs):
-    # survey
-    survey = Survey.objects.get(pk=1)
-
-    # country
-    country = Country.objects.get(pk=kwargs['country_id'])
-
-    # profile
-    try:
-        profile = Profiles.objects.get(country_id=country.id)
-    except:
-        profile = []
-        pass
-
-   # questions
-    questions = QuestionList.objects.filter(section_id=2)
-
-    # context
-    context = {
-        "survey": survey,
-        "questions": questions,
-        "country": country,
-        "profile": profile,
-        "user": profile.user
-    }
-    # render view
-    return render(request, "sections/two.html", context)
-
-
-# section three
-@login_required(login_url='/login')
-def section_three(request, **kwargs):
-    # survey
-    survey = Survey.objects.get(pk=1)
-
-    # country
-    country = Country.objects.get(pk=kwargs['country_id'])
-
-    # profile
-    try:
-        profile = Profiles.objects.get(country_id=country.id)
-    except:
-        profile = []
-        pass
-
-   # questions
-    questions = QuestionList.objects.filter(section_id=3)
-
-    # context
-    context = {
-        "survey": survey,
-        "questions": questions,
-        "country": country,
-        "profile": profile,
-        "user": profile.user
-    }
-    # render view
-    return render(request, "sections/three.html", context)
-
-
-# section four
-@login_required(login_url='/login')
-def section_four(request, **kwargs):
-    # survey
-    survey = Survey.objects.get(pk=1)
-
-    # country
-    country = Country.objects.get(pk=kwargs['country_id'])
-
-    # profile
-    try:
-        profile = Profiles.objects.get(country_id=country.id)
-    except:
-        profile = []
-        pass
-
-   # questions
-    questions = QuestionList.objects.filter(section_id=4)
-
-    # context
-    context = {
-        "survey": survey,
-        "questions": questions,
-        "country": country,
-        "profile": profile,
-        "user": profile.user
-    }
-    # render view
-    return render(request, "sections/four.html", context)
-
-
-# section five
-@login_required(login_url='/login')
-def section_five(request, **kwargs):
-    # survey
-    survey = Survey.objects.get(pk=1)
-
-    # country
-    country = Country.objects.get(pk=kwargs['country_id'])
-
-    # profile
-    try:
-        profile = Profiles.objects.get(country_id=country.id)
-    except:
-        profile = []
-        pass
-
-   # questions
-    questions = QuestionList.objects.filter(section_id=5)
-
-    # context
-    context = {
-        "survey": survey,
-        "questions": questions,
-        "country": country,
-        "profile": profile,
-        "user": profile.user
-    }
-    # render view
-    return render(request, "sections/five.html", context)
-
-
-# section six
-@login_required(login_url='/login')
-def section_six(request, **kwargs):
-    # survey
-    survey = Survey.objects.get(pk=1)
-
-    # country
-    country = Country.objects.get(pk=kwargs['country_id'])
-
-    # profile
-    try:
-        profile = Profiles.objects.get(country_id=country.id)
-    except:
-        profile = []
-        pass
-
-   # questions
-    questions = QuestionList.objects.filter(section_id=6)
-
-    # context
-    context = {
-        "survey": survey,
-        "questions": questions,
-        "country": country,
-        "profile": profile,
-        "user": profile.user
-    }
-    # render view
-    return render(request, "sections/six.html", context)
-
-# ===========================================================
-# SURVEY 2
-# ===========================================================
-# general information
-
-
-@login_required(login_url='/login')
-def general_info(request, **kwargs):
-    # survey
-    survey = Survey.objects.get(pk=2)
-
-    # section
-    section = Section.objects.get(pk=10)
-
-    # country
-    country = Country.objects.get(pk=kwargs['country_id'])
-
-    # attachments
-    attachments = Attachments.objects.select_related(
-        'ansbank', 'ansbank__question', 'ansbank__question__section').filter(ansbank__question__section__survey_id=2, ansbank__country_id=country.id)
-
-    # profile
-    try:
-        profile = Profiles.objects.get(country_id=country.id)
-        user = profile.user
-    except:
-        profile = []
-        user = []
-        pass
-
-    # context
-    context = {
-        "survey": survey,
+        "section_id": section_id,
         "section": section,
-        "country": country,
+        "prev_section_id": prev_section_id,
+        "next_section_id": next_section_id,
         "profile": profile,
-        "user": user, 
-        "attachments" : attachments
-    }
-    # render view
-    return render(request, "sections/p_general_info.html", context)
-
-
-# download attachments
-# def download_attachments(request):
-
-
-# metrics
-@login_required(login_url='/login')
-def metrics(request, **kwargs):
-    # survey
-    survey = Survey.objects.get(pk=2)
-
-    # section
-    section = Section.objects.get(pk=7)
-
-    # country
-    country = Country.objects.get(pk=kwargs['country_id'])
-
-    # profile
-    try:
-        profile = Profiles.objects.get(country_id=country.id)
-        user = profile.user
-    except:
-        profile = []
-        user = []
-        pass
-
-    # context
-    context = {
-        "survey": survey,
-        "section": section,
+        "user": user,
         "country": country,
-        "profile": profile,
-        "user": user
-    }
+        "questions": questions
+    } 
+
     # render view
-    return render(request, "sections/p_metrics.html", context)
-
-# preference
+    return render(request, "questions.html", context)
 
 
-@login_required(login_url='/login')
-def preference(request, **kwargs):
-    # survey
-    survey = Survey.objects.get(pk=2)
+#calculate next section
+def next_section(survey_id, section_id):
+    #last id
+    last_section = Section.objects.filter(survey_id = survey_id).order_by('-code')[:1]
 
-    # section
-    section = Section.objects.get(pk=8)
+    print(last_section)
 
-    # country
-    country = Country.objects.get(pk=kwargs['country_id'])
+    #section
+    section = Section.objects.get(pk=section_id)
 
-    # profile
-    try:
-        profile = Profiles.objects.get(country_id=country.id)
-        user = profile.user
-    except:
-        profile = []
-        user = []
-        pass
+    if section.code < last_section[0].code:
+        next_code = section.code + 1
+        section = Section.objects.filter(survey_id=survey_id, code = next_code).first()
+        return section.id
 
-    # context
-    context = {
-        "survey": survey,
-        "section": section,
-        "country": country,
-        "profile": profile,
-        "user": user
-    }
-    # render view
-    return render(request, "sections/p_preference.html", context)
+#calculate previous section
+def prev_section(survey_id, section_id):
+    #first id
+    first_section = Section.objects.filter(survey_id = survey_id).order_by('code')[:1]
 
+    print(first_section)
 
-# information
-@login_required(login_url='/login')
-def information(request, **kwargs):
-    # survey
-    survey = Survey.objects.get(pk=2)
+    #section
+    section = Section.objects.get(pk=section_id)
 
-    # section
-    section = Section.objects.get(pk=9)
+    if section.code > first_section[0].code:
+        prev_code = section.code - 1
+        section = Section.objects.filter(survey_id=survey_id, code = prev_code).first()
+        return section.id
 
-    # country
-    country = Country.objects.get(pk=kwargs['country_id'])
-
-    # profile
-    try:
-        profile = Profiles.objects.get(country_id=country.id)
-        user = profile.user
-    except:
-        profile = []
-        user = []
-        pass
-
-    # context
-    context = {
-        "survey": survey,
-        "section": section,
-        "country": country,
-        "profile": profile,
-        "user": user
-    }
-    # render view
-    return render(request, "sections/p_information.html", context)
 
 # ==========================================================
 # MANAGEMENT CALLBACK FUNCTIONS
 # ==========================================================
 # send incomplete submission alert
-
-
 @login_required(login_url='/login')
 def send_incomplete_submission_alert(request, **kwargs):
     # check for incomplete submission
@@ -499,8 +237,6 @@ def export_csv(request, **kwargs):
     return response
 
 # export csv
-
-
 def export_xls(request, **kwargs):
     # survey
     survey = Survey.objects.get(pk=kwargs['survey_id'])
